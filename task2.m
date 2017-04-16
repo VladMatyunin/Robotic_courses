c0725f9a9da8a97a75d162251822221e2df3e0d1
Vladislav <vlad.matyunin96@gmail.com>
2017-04-16 23:25:28 +0300

    Home task almost compete

---
+++ task2.m
@@ -0,0 +1,9 @@
+l2(1) = Link([0, 0, 0, -pi/2, 1]);
+l2(1).qlim = [0 2];
+l2(2) = Link([0, 1, 0, pi/2, 1]);
+l2(2).qlim = [0 2];
+l2(3) = Link([0, 0, 0, 0, 0]);
+r = SerialLink(l2, 'name', 'Figure1');
+
+r.plot([0,1,2],'workspace', [-0.5 1.5 -2 3 -1 5]);
+r.teach()
